b040390ecbdd23ab31c1d049173443c7caeda77c
syeddxm <syed@dxm.to>
2019-08-16 15:11:57 -0400

    first commit

---
+++ index.ios.js
@@ -0,0 +1,10 @@
+import { AppRegistry } from 'react-native';
+import App from './src/app';
+
+AppRegistry.registerComponent('tech_stack', () => App)
+
+
+
+
+
+
---
+++ src/components/common/Header.js
@@ -0,0 +1,38 @@
+// import libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// make a component
+const Header = (props) => {
+	const { textStyle, viewStyle } = styles;
+
+	return (
+		<View style={viewStyle}>
+			<Text style={textStyle}>{ props.headerText }</Text>
+		</View>
+	);
+};
+
+
+const styles = {
+	viewStyle: {
+		backgroundColor: '#fafafa',
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60,
+		paddingTop: 15,
+		shadowColor: 'black',
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.5,
+		elevation: 2,
+		position: 'relative',
+	},
+	textStyle: {
+		fontSize: 20
+	}
+};
+
+// make component available to other parts of the app
+
+export { Header };
+
